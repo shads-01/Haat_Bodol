8e2763ede65bf4fc918e85a43f3c473eb0750804
samiarpita <arpitarahmansamia@gmail.com>
2025-09-01 20:02:55 +0600

    extra item schema code

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,28 +1,29 @@
-// import mongoose from "mongoose";
-
-// const itemSchema = new mongoose.Schema(
-//     {
-//         title: {type: String, required: true},
-//         description: {type: String, required: true},
-//     }
-// )
-// export default mongoose.model("Item", itemSchema);
-
-
-
-import { Category } from "@mui/icons-material";
 import mongoose from "mongoose";
 
-const itemSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  category: String,
-  condition: String,
-  status: String,
-  location: String,
-  postedTime: String,
-  image: String,
-});
+const itemSchema = new mongoose.Schema(
+    {
+        title: {type: String, required: true},
+        description: {type: String, required: true},
+    }
+)
+export default mongoose.model("Item", itemSchema);
 
-const Item = mongoose.model("Item", itemSchema);
-export default Item;
+
+// import mongoose from "mongoose";
+
+// const itemSchema = new mongoose.Schema({
+//   name: String,
+//   description: String,
+//   category: String,
+//   condition: String,
+//   status: String,
+//   location: String,
+//   postedTime: String,
+//   image: String,
+// });
+
+// const Item = mongoose.model("Item", itemSchema);
+// export default Item;
+
+
+
