2a31e687ce2b413d0a5c70312369420ab485af57
Hrittika Saha <68088694+hrittikaaa@users.noreply.github.com>
2025-09-02 17:03:16 +0600

    Fixed item fetch issues.

--- server/routes/items.js
+++ server/routes/items.js
@@ -7,9 +7,6 @@ const router = Router();
 router.post("/", createAnItem);
 
 // GET all items
-// router.get("/", getAllItems);
-
-// GET /api/items → fetch all items
 router.get("/", getAllItems);
 
 export default router;
